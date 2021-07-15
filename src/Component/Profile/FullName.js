d794f06a4d31c43bcbf7d8e1d50f2a07ffb8cfb8
Rami3344 <tounsi.csscool123@gmail.com>
2021-07-15 17:46:07 +0100

    components

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullName = () => {
+    return (
+        <div>
+            <h2>Full Name: Rami Tounsi</h2>
+        </div>
+    )
+}
+
+export default FullName
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Photo from './me.jpg'
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={Photo}  />
+        </div>
+    )
+}
+
+export default ProfilePhoto
